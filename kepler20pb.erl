0ff0b24cb57ef8bb93667661579b699b268b8ee8
Augusto Mentz <augustohmn.tech@gmail.com>
2021-11-18 23:27:03 -0300

    eu sou um genio

--- kepler20pb.erl
+++ kepler20pb.erl
@@ -1,40 +1,40 @@
 -module(kepler20pb).
--export([start/0, kepler20pb/1, molecule/1, generate_molecules/0, ready_to_turn_water/0]).
+-export([start/0, kepler20pb/2, molecule/2, generate_molecules/1, ready_to_turn_water/1]).
 
-kepler20pb(0) ->
+kepler20pb(_, 0) ->
     io:format("Finished.~n", []);
-kepler20pb(N_MOLECULES) ->
-    generate_molecules(),
+kepler20pb(RttwPID, N_MOLECULES) ->
+    generate_molecules(RttwPID),
 
     timer:sleep(500),
 
-    kepler20pb(N_MOLECULES - 1).
+    kepler20pb(RttwPID, N_MOLECULES - 1).
 
-ready_to_turn_water() ->
-    List = [],
+ready_to_turn_water(State) ->
     receive
-        {ready, List} ->
-            erlang:insert_element(1, List, 'z'),
-            % lists:append(List, {Molecule_PID, Molecule_Type}),
-            io:format("~p", [element(1, List)])
+        {Type} ->
+            NewList = lists:append(State, Type),
+            io:format("~p ~n", [NewList]),
+            
+            ready_to_turn_water(NewList)
     end.
 
-molecule(Molecule_Type) ->
+molecule(Molecule_Type, RttwPID) ->
     io:format("A molecule ~p of ~s is energizing ~n", [self(), Molecule_Type]),
-    timer:sleep((rand:uniform(20) + 10) * 1000),
+    timer:sleep((rand:uniform(2) + 4) * 1000),
     io:format("The molecule ~p of ~s is energized.~n", [self(), Molecule_Type]),
 
-    RttwPID = spawn(kepler20pb, ready_to_turn_water, []),
-    RttwPID ! { ready, {self(), Molecule_Type} }.
+    RttwPID ! { [Molecule_Type] }.
 
-generate_molecules() ->
+generate_molecules(RttwPID) ->
     Random = rand:uniform(50),
 
     if 
         Random > 25 ->
-            spawn(kepler20pb, molecule, ["OXYGEN"]);
+            spawn(kepler20pb, molecule, ["OXYGEN", RttwPID]);
         true -> 
-            spawn(kepler20pb, molecule, ["HYDROGEN"])
+            spawn(kepler20pb, molecule, ["HYDROGEN", RttwPID])
     end.
 start() ->
-    kepler20pb(20).
+    RttwPID = spawn(kepler20pb, ready_to_turn_water, [[]]),
+    kepler20pb(RttwPID, 3).
