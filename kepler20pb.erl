a3f86e452f5a696200662746ff2ef0d89882f687
Augusto Mentz <augustohmn.tech@gmail.com>
2021-11-26 00:49:07 -0300

    wip: generating water molecules

--- kepler20pb.erl
+++ kepler20pb.erl
@@ -1,41 +1,74 @@
 -module(kepler20pb).
--export([start/0, kepler20pb/2, molecule/2, generate_molecules/1, ready_to_turn_water/1]).
+-export([start/1, kepler20pb/2, molecule/0, generate_molecules/1, ready_to_turn_water/2]).
 -import(lists, [map/2]).
 
-kepler20pb(_, 0) ->
-    io:format("No have more molecules of Oxygens and Hydrogens at Kepler82.~n", []);
-kepler20pb(RttwPID, N_MOLECULES) ->
+kepler20pb(RttwPID, Time) ->
     generate_molecules(RttwPID),
 
-    timer:sleep(500),
+    timer:sleep(Time * 1000),
 
-    kepler20pb(RttwPID, N_MOLECULES - 1).
-
-ready_to_turn_water(State) ->
+    kepler20pb(RttwPID, Time).
+ready_to_turn_water(OxygenList, HydrogenList) ->
     receive
-        {Type} ->
-            NewList = lists:append(State, Type),
+        {oxygen, OPid} ->
+            NewOxygenList = OxygenList ++ [{oxygen, OPid}],
 
-            map(fun(X) -> io:format("~s ~n", [X]) end, NewList),
+            if (length(NewOxygenList) > 0) and (length(HydrogenList) > 1) ->
+                    [Oxygen | OxygenListTail] = NewOxygenList,
+                    [HydrogenOne, HydrogenTwo | HydrogenListTail] = HydrogenList,
 
-            ready_to_turn_water(NewList)
+                    io:format("~n", []),
+                    io:format("The molecules (~p/~p) (~p/~p) (~p/~p) ~n", [element(1, HydrogenOne), element(2, HydrogenOne), element(1, HydrogenTwo), element(2, HydrogenTwo), element(1, Oxygen), element(2, Oxygen)]),
+                    io:format("~n", []),
+                    
+                    ready_to_turn_water(OxygenListTail, HydrogenListTail);
+                true -> true
+            end,
+
+            ready_to_turn_water(NewOxygenList, HydrogenList);
+        {hydrogen, HPid} ->
+            NewHydrogenList = HydrogenList ++ [{hydrogen, HPid}],
+
+            if (length(NewHydrogenList) > 1) and (length(OxygenList) > 0) ->
+                    [Oxygen | OxygenListTail] = OxygenList,
+                    [HydrogenOne, HydrogenTwo | HydrogenListTail] = NewHydrogenList, 
+
+                    io:format("~n", []),
+                    io:format("The molecules (~p/~p) (~p/~p) (~p/~p) TURN WATER!~n", [element(1,HydrogenOne), element(2, HydrogenOne), element(1, HydrogenTwo), element(2, HydrogenTwo), element(1, Oxygen), element(2, Oxygen)]),
+                    io:format("~n", []),
+
+                    ready_to_turn_water(OxygenListTail, HydrogenListTail);
+                true -> true
+            end,
+
+            ready_to_turn_water(OxygenList, NewHydrogenList)
     end.
-molecule(Molecule_Type, RttwPID) ->
-    io:format("A molecule ~p of ~s is energizing ~n", [self(), Molecule_Type]),
-    timer:sleep((rand:uniform(2) + 4) * 1000),
-    io:format("The molecule ~p of ~s is energized.~n", [self(), Molecule_Type]),
+molecule() ->
+    receive
+        {oxygen, RttwPID} ->
+            io:format("CREATE: Molecule ~p of ~p is energizing ~n", [self(), oxygen]),
+            timer:sleep((rand:uniform(20) + 10) * 1000),
+            io:format("ENERGIZED: The molecule ~p of ~p is energized.~n", [self(), oxygen]), 
 
-    RttwPID ! { [Molecule_Type] }.
+            RttwPID ! {oxygen, self()};
+        {hydrogen, RttwPID} ->
+            io:format("CREATE: Molecule ~p of ~p is energizing ~n", [self(), hydrogen]),
+            timer:sleep((rand:uniform(20) + 10) * 1000),
+            io:format("ENERGIZED: The molecule ~p of ~p is energized.~n", [self(), hydrogen]),
 
+            RttwPID ! {hydrogen, self()}
+    end.
 generate_molecules(RttwPID) ->
     Random = rand:uniform(50),
 
     if 
         Random > 25 ->
-            spawn(kepler20pb, molecule, ["OXYGEN", RttwPID]);
+            MOPid = spawn(kepler20pb, molecule, []),
+            MOPid ! { oxygen, RttwPID };
         true -> 
-            spawn(kepler20pb, molecule, ["HYDROGEN", RttwPID])
+            MHPid = spawn(kepler20pb, molecule, []),
+            MHPid ! { hydrogen, RttwPID }
     end.
-start() ->
-    RttwPID = spawn(kepler20pb, ready_to_turn_water, [[]]),
-    kepler20pb(RttwPID, 3).
+start(Time) ->
+    RttwPID = spawn(kepler20pb, ready_to_turn_water, [[], []]),
+    kepler20pb(RttwPID, Time).
