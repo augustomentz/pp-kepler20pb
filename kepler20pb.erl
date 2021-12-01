8dd50a04010a9cf6071fee33b71b30a0272f7b72
Augusto Mentz <augustohmn.tech@gmail.com>
2021-12-01 13:47:38 -0300

    wip: some changes

--- kepler20pb.erl
+++ kepler20pb.erl
@@ -1,27 +1,23 @@
 -module(kepler20pb).
--export([start/1, kepler20pb/2, molecule/0, generate_molecules/1, ready_to_turn_water/2]).
+-export([start/1, kepler20pb/3, molecule/0, generate_molecules/1, ready_to_turn_water/2, create_water/2]).
 -import(lists, [map/2]).
-
-kepler20pb(RttwPID, Time) ->
+kepler20pb(_,0,_) ->
+    io:format("Finished", []);
+kepler20pb(RttwPID, N_MOLCULES, Time) ->
     generate_molecules(RttwPID),
 
     timer:sleep(Time * 1000),
 
-    kepler20pb(RttwPID, Time).
+    kepler20pb(RttwPID, N_MOLCULES - 1, Time).
 ready_to_turn_water(OxygenList, HydrogenList) ->
     receive
         {oxygen, OPid} ->
             NewOxygenList = OxygenList ++ [{oxygen, OPid}],
 
             if (length(NewOxygenList) > 0) and (length(HydrogenList) > 1) ->
-                    [Oxygen | OxygenListTail] = NewOxygenList,
-                    [HydrogenOne, HydrogenTwo | HydrogenListTail] = HydrogenList,
+                OxygenHydrogenTuple = create_water(NewOxygenList, HydrogenList),
 
-                    io:format("~n", []),
-                    io:format("The molecules (~p/~p) (~p/~p) (~p/~p) ~n", [element(1, HydrogenOne), element(2, HydrogenOne), element(1, HydrogenTwo), element(2, HydrogenTwo), element(1, Oxygen), element(2, Oxygen)]),
-                    io:format("~n", []),
-                    
-                    ready_to_turn_water(OxygenListTail, HydrogenListTail);
+                ready_to_turn_water(element(1, OxygenHydrogenTuple), element(2, OxygenHydrogenTuple));     
                 true -> true
             end,
 
@@ -29,20 +25,24 @@ ready_to_turn_water(OxygenList, HydrogenList) ->
         {hydrogen, HPid} ->
             NewHydrogenList = HydrogenList ++ [{hydrogen, HPid}],
 
-            if (length(NewHydrogenList) > 1) and (length(OxygenList) > 0) ->
-                    [Oxygen | OxygenListTail] = OxygenList,
-                    [HydrogenOne, HydrogenTwo | HydrogenListTail] = NewHydrogenList, 
+            if (length(OxygenList) > 0) and (length(HydrogenList) > 1) ->
+                OxygenHydrogenTuple = create_water(OxygenList, NewHydrogenList),
 
-                    io:format("~n", []),
-                    io:format("The molecules (~p/~p) (~p/~p) (~p/~p) TURN WATER!~n", [element(1,HydrogenOne), element(2, HydrogenOne), element(1, HydrogenTwo), element(2, HydrogenTwo), element(1, Oxygen), element(2, Oxygen)]),
-                    io:format("~n", []),
-
-                    ready_to_turn_water(OxygenListTail, HydrogenListTail);
+                ready_to_turn_water(element(1, OxygenHydrogenTuple), element(2, OxygenHydrogenTuple));
                 true -> true
             end,
 
             ready_to_turn_water(OxygenList, NewHydrogenList)
     end.
+create_water(OxygenList, HydrogenList) ->
+    [Oxygen | OxygenListTail] = OxygenList,
+    [HydrogenOne, HydrogenTwo | HydrogenListTail] = HydrogenList,
+
+    io:format("~n", []),
+    io:format("The molecules (~p/~p) (~p/~p) (~p/~p) are created a new water molecule!~n", [element(1, HydrogenOne), element(2, HydrogenOne), element(1, HydrogenTwo), element(2, HydrogenTwo), element(1, Oxygen), element(2, Oxygen)]),
+    io:format("~n", []),
+
+    {OxygenListTail, HydrogenListTail}.
 molecule() ->
     receive
         {oxygen, RttwPID} ->
@@ -71,4 +71,4 @@ generate_molecules(RttwPID) ->
     end.
 start(Time) ->
     RttwPID = spawn(kepler20pb, ready_to_turn_water, [[], []]),
-    kepler20pb(RttwPID, Time).
+    kepler20pb(RttwPID, 10, Time).
